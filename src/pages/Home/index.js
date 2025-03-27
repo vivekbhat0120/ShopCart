ba189c6c979744d2d7da4724368d2ab302b43aed
vivekbhat0120 <vivekbhat0120@gmail.com>
2025-03-27 19:14:56 +0530

    Updated comments in home folder

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,92 +1,63 @@
-import React from "react";
-import Slider from "./slider/index";
-import CatSlider from "../../components/catSlider";
-import Banners from "../../components/banners/banner";
-import Product from "../../components/product/product";
+import React from "react";  // Import React library to use JSX
+import Slider from "./slider/index";  // Import the Slider component
+import CatSlider from "../../components/catSlider";  // Import category slider component
+import Banners from "../../components/banners/banner";  // Import banner component
+import Product from "../../components/product/product";  // Import Product component
 
-import "./style.css";
+import "./style.css";  // Import stylesheet for styling
 
+// Define the Home component
 const Home = () => {
     return (
       <>
+        {/* Render the slider component */}
         <Slider/>
+
+        {/* Render the category slider component */}
         <CatSlider/>
+
+        {/* Render the banners component */}
         <Banners/>
 
+        {/* Section for displaying popular products */}
         <section className="homeProduct">
             <div className="container-fluid">
+              
+              {/* Header section with filter tabs */}
               <div className="d-flex align-items-center">
                 <h4 className="hd">Popular Products</h4>
+
+                {/* Filter tabs for product categories */}
                 <ul className="list list-inline ml-auto filterTab">
-                  <li className="list-inline-item">
-                    <a className="cursor">All</a>
-                  </li>
-                  <li className="list-inline-item">
-                    <a className="cursor">Electronics</a>
-                  </li>
-                  <li className="list-inline-item">
-                    <a className="cursor">Books</a>
-                  </li>
-                  <li className="list-inline-item">
-                    <a className="cursor">Shoes</a>
-                  </li>
-                  <li className="list-inline-item">
-                    <a className="cursor">Clothing</a>
-                  </li>
-                  <li className="list-inline-item">
-                    <a className="cursor">Medicines</a>
-                  </li>
-                  <li className="list-inline-item">
-                    <a className="cursor">Fruits & Vegetables</a>
-                  </li>
-                  </ul>
-                </div>
+                  <li className="list-inline-item"><a className="cursor">All</a></li>
+                  <li className="list-inline-item"><a className="cursor">Electronics</a></li>
+                  <li className="list-inline-item"><a className="cursor">Books</a></li>
+                  <li className="list-inline-item"><a className="cursor">Shoes</a></li>
+                  <li className="list-inline-item"><a className="cursor">Clothing</a></li>
+                  <li className="list-inline-item"><a className="cursor">Medicines</a></li>
+                  <li className="list-inline-item"><a className="cursor">Fruits & Vegetables</a></li>
+                </ul>
+              </div>
 
-                <div className="row productRow">
-                  <div className="item">
-                    <Product/>
-                  </div>
-
-                  <div className="item">
-                    <Product/>
-                  </div>
-
-                  <div className="item">
-                    <Product/>
-                  </div>
-
-                  <div className="item">
-                    <Product/>
-                  </div>
-
-                  <div className="item">
-                    <Product/>
-                  </div>
-
-                  <div className="item">
-                    <Product/>
-                  </div>
-
-                  <div className="item">
-                    <Product/>
-                  </div>
-
-                  <div className="item">
-                    <Product/>
-                  </div>
-
-                  <div className="item">
-                    <Product/>
-                  </div>
-
-                  <div className="item">
-                    <Product/>
-                  </div>
-                </div>
+              {/* Grid layout for product items */}
+              <div className="row productRow">
+                {/* Display multiple Product components */}
+                <div className="item"><Product/></div>
+                <div className="item"><Product/></div>
+                <div className="item"><Product/></div>
+                <div className="item"><Product/></div>
+                <div className="item"><Product/></div>
+                <div className="item"><Product/></div>
+                <div className="item"><Product/></div>
+                <div className="item"><Product/></div>
+                <div className="item"><Product/></div>
+                <div className="item"><Product/></div>
+              </div>
             </div>
         </section>
       </>
     )
 }
 
+// Export the Home component for use in other parts of the application
 export default Home;
