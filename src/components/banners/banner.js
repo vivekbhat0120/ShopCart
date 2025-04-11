8df6596f1903d9bee22d54571f73d63f34d33786
vivekbhat0120 <vivekbhat0120@gmail.com>
2025-04-11 09:25:19 +0530

    03 commented for components code

--- src/components/banners/banner.js
+++ src/components/banners/banner.js
@@ -7,31 +7,35 @@ import Ban2 from "../../assets/images/ban2.png";
 import Ban3 from "../../assets/images/ban3.png";
 
 const Banners = () => {
-    return (
-        <section className="bannerSection">
-            <div className="container-fluid">
-                <div className="row">
-                    <div className="col">
-                        <div className="box">
-                            <img src={Ban1} className="w-100" alt="banner" loading="lazy"/>
-                        </div>
-                    </div>
-
-                    <div className="col">
-                        <div className="box">
-                            <img src={Ban2} className="w-100" alt="banner" loading="lazy"/>
-                        </div>
-                    </div>
-
-                    <div className="col">
-                        <div className="box">
-                            <img src={Ban3} className="w-100" alt="banner" loading="lazy"/>
-                        </div>
-                    </div>
-                </div>
+  return (
+    // Banner section containing three banners
+    <section className="bannerSection">
+      <div className="container-fluid">
+        <div className="row">
+          {/* First banner */}
+          <div className="col">
+            <div className="box">
+              <img src={Ban1} className="w-100" alt="banner" loading="lazy" />
             </div>
-        </section>
-    )
-}
+          </div>
+
+          {/* Second banner */}
+          <div className="col">
+            <div className="box">
+              <img src={Ban2} className="w-100" alt="banner" loading="lazy" />
+            </div>
+          </div>
+
+          {/* Third banner */}
+          <div className="col">
+            <div className="box">
+              <img src={Ban3} className="w-100" alt="banner" loading="lazy" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default Banners;
--- src/components/catSlider/index.js
+++ src/components/catSlider/index.js
@@ -15,47 +15,50 @@ import Trend9 from "../../assets/images/medicine.png";
 import Trend10 from "../../assets/images/furniture.png";
 
 const CatSlider = () => {
-    const settings = {
-        dots: false,
-        infinite: true,
-        speed: 3000,              // How long the animation takes
-        cssEase: "linear",        // Ensures smooth motion
-        slidesToShow: 8,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 0,
-        arrows: false
-    };
+  // Slider settings for the carousel
+  const settings = {
+    dots: false, // Disable navigation dots
+    infinite: true, // Enable infinite scrolling
+    speed: 3000, // Animation duration in milliseconds
+    cssEase: "linear", // Smooth linear animation
+    slidesToShow: 8, // Number of slides visible at a time
+    slidesToScroll: 1, // Number of slides to scroll at a time
+    autoplay: true, // Enable autoplay
+    autoplaySpeed: 0, // Autoplay speed (0 for continuous scrolling)
+    arrows: false, // Disable navigation arrows
+  };
 
-    return (
-        <section className="catSliderSection">
-            <div className="container-fluid">
-                <h3 className="hd">Trending Categories</h3>
-                <Slider {...settings} className="catSliderMain">
-                    {[
-                        { img: Trend1, name: "Gadgets" },
-                        { img: Trend2, name: "Toys" },
-                        { img: Trend3, name: "Tools" },
-                        { img: Trend4, name: "Shoes" },
-                        { img: Trend5, name: "Cloth" },
-                        { img: Trend6, name: "Jewelry" },
-                        { img: Trend7, name: "Games" },
-                        { img: Trend8, name: "Books" },
-                        { img: Trend9, name: "Medicine" },
-                        { img: Trend10, name: "Furniture" }
-                    ].map((item, index) => (
-                        <div className="catSliderItem" key={index}>
-                            <div className="info">
-                                <img src={item.img} alt={item.name} loading="lazy"/>
-                                <h5>{item.name}</h5>
-                                <p>1000+ Items</p>
-                            </div>
-                        </div>
-                    ))}
-                </Slider>
+  return (
+    // Section for the category slider
+    <section className="catSliderSection">
+      <div className="container-fluid">
+        <h3 className="hd">Trending Categories</h3>
+        <Slider {...settings} className="catSliderMain">
+          {/* Map through the category items and render each */}
+          {[
+            { img: Trend1, name: "Gadgets" },
+            { img: Trend2, name: "Toys" },
+            { img: Trend3, name: "Tools" },
+            { img: Trend4, name: "Shoes" },
+            { img: Trend5, name: "Cloth" },
+            { img: Trend6, name: "Jewelry" },
+            { img: Trend7, name: "Games" },
+            { img: Trend8, name: "Books" },
+            { img: Trend9, name: "Medicine" },
+            { img: Trend10, name: "Furniture" },
+          ].map((item, index) => (
+            <div className="catSliderItem" key={index}>
+              <div className="info">
+                <img src={item.img} alt={item.name} loading="lazy" />
+                <h5>{item.name}</h5>
+                <p>1000+ Items</p>
+              </div>
             </div>
-        </section>
-    );
+          ))}
+        </Slider>
+      </div>
+    </section>
+  );
 };
 
 export default CatSlider;
